294bdd4a07f10252f7b6bef602fb5c0136fa2f70
Benjie Gillam <benjie@jemjie.com>
2024-11-23 13:28:59 +0000

    Remove unnecessary appendPlugins

--- __tests__/integration/schema/ignoreIndexes.test.js
+++ __tests__/integration/schema/ignoreIndexes.test.js
@@ -1,11 +1,9 @@
 // @ts-check
 const core = require("./core");
-const { default: PgOrderByRelatedPlugin } = require("../../../dist/index.js");
 
 test(
   "prints a schema with `ignoreIndexes: false`",
   core.test(["p"], {
-    appendPlugins: [PgOrderByRelatedPlugin],
     disableDefaultMutations: true,
     legacyRelations: "omit",
     ignoreIndexes: false,
--- __tests__/integration/schema/orderByRelated.test.js
+++ __tests__/integration/schema/orderByRelated.test.js
@@ -1,11 +1,9 @@
 // @ts-check
 const core = require("./core");
-const { default: PgOrderByRelatedPlugin } = require("../../../dist/index.js");
 
 test(
   "prints a schema with the order-by-related plugin",
   core.test(["p"], {
-    appendPlugins: [PgOrderByRelatedPlugin],
     disableDefaultMutations: true,
     legacyRelations: "omit",
   })
